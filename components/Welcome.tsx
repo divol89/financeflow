f8f5f8c44dea05f619d850c22dce1d43d22223d0
divol89 <diablero992@gmail.com>
2023-07-03 21:14:11 +0200

    url learn more

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -12,7 +12,7 @@ const Welcome = () => {
           Finance <br />
           Flow<strong className="text-yellow-500">.</strong>
         </h1>
-        <Link href="http://localhost:3000/whitepaper">
+        <Link href="https://finance-flow.xyz/whitepaper">
       <button className="mt-4.5 mb-5 bg-yellow-500 text-white py-0.7 px-4 rounded-lg md:py-0.6 md:px-3 lg:ml-48 hover:bg-blue-700 transition-all">
         <strong className="text-sm md:text-2xl">Learn More</strong>
       </button>
