445d179c67ae88210badf792a36a7f27ed5b754c
divol89 <diablero992@gmail.com>
2024-08-27 16:26:32 +0200

    rebrand colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,3 @@
-// import React, { useState } from "react";
-// import Head from "next/head";
-// import Navbar from "../components/Navbar";
-// import ContactUs from "../components/ContactUs";
-// import Welcome from "../components/Welcome";
-// import TokenGrid from "../components/TokenGrid";
-// import Discover from "../components/Discover";
-// import Footer from "../components/Footer";
-
-// export default function Home() {
-//   const [selectedPoolAddress, setSelectedPoolAddress] = useState(null);
-
-//   // Usar selectedPoolAddress en alguna parte de tu componente
-//   console.log(selectedPoolAddress);
-//   return (
-//     <div className=" flex-shrink bg-gradient-to-t from-black to-gray-400">
-//       <Head>
-//         <title>FinanceFlow</title>
-//         <meta name="web3 Portal" content="Web3 utility Dapp" />
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-//       <Navbar />
-//       {/* Otros componentes de tu aplicación */}
-//       <TokenGrid setSelectedPoolAddress={setSelectedPoolAddress} />
-//       <div className="bg-fondo-waves bg-cover bg-no-repeat bg-center">
-//         <Welcome />
-//       </div>
-//       <Discover />
-//       <ContactUs />
-//       <Footer />
-//     </div>
-//   );
-// }
-
 import React, { useState } from "react";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
@@ -54,7 +20,7 @@ export default function Home() {
   };
 
   return (
-    <div className="flex-shrink bg-gradient-to-t from-black to-gray-400">
+    <div className="flex-shrink bg-gradient-to-t from-black to-gray-700">
       <Head>
         <title>FinanceFlow</title>
         <meta name="web3 Portal" content="Web3 utility Dapp" />
